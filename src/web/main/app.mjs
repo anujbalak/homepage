3e75215f7c04f6408cc600eb4038f8f149b2c9d1
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-12 07:25:20 +0530

    Load about file

--- src/web/main/app.mjs
+++ src/web/main/app.mjs
@@ -1,10 +1,12 @@
 import { renderHeader } from "./header.mjs";
 import { rednerPage } from "./page.mjs";
 import { renderProjects } from "./projects.mjs";
-
+import { renderAbout } from "./about.mjs";
 export function renderWeb() {
     rednerPage();
     renderHeader();
+    renderAbout();
     renderProjects();
+
 }
 
