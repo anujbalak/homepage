11859570b03f2ed11d2073b1c7e8a6c2c6e23254
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-12 09:40:12 +0530

    Create main file to manage main section of the page

---
+++ src/web/main/main.mjs
@@ -0,0 +1,16 @@
+import { renderProjects } from "./projects.mjs";
+import { renderAbout } from "./profile.mjs";
+
+export function renderMainSection() {
+    main();
+    renderAbout();
+    renderProjects()
+}
+
+function main() {
+    const main = document.createElement('main');
+    main.className = 'main';
+
+    const page = document.querySelector('div.page');
+    page.appendChild(main);
+}
