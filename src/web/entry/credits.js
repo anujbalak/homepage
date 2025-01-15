c553a05b9167dc5bd76c6391a2dbf8b19efe9a0e
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-15 15:37:16 +0530

    Add observation api

--- src/web/entry/credits.js
+++ src/web/entry/credits.js
@@ -1,1 +1,23 @@
 import "../main/credits/credits.css";
+
+function animateCredits() { 
+    const animatedElements = document.querySelectorAll('div');
+    animatedElements.forEach(element => {
+        element.classList.remove('animated');
+    })
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('animated');
+            return;
+          }
+          entry.target.classList.remove('animated');
+        });
+      });
+      
+    const elements = document.querySelectorAll('div');
+    elements.forEach((element) => observer.observe(element));
+}
+
+animateCredits()
